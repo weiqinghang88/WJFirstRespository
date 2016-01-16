05aafab81c215ab790e821966b7f0c2fb836e10e
bringbird <weimingjie@weimingjiedeMacBook-Pro.local>
2016-01-16 19:14:31 +0800

    first commit
    first commit

---
+++ FirstRespository/FirstRespository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstRespository
+//
+//  Created by bringbird on 16/1/16.
+//  Copyright © 2016年 韦明杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstRespository/FirstRespository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstRespository
+//
+//  Created by bringbird on 16/1/16.
+//  Copyright © 2016年 韦明杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
